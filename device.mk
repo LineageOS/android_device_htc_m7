e0593363b777c6640858e9ace097f1d0ca7dd9cc
Matt Mower <mowerm@gmail.com>
2017-01-11 18:05:36 -0600

    m7: Re-add FM radio support
    * FMRecord is not available, so omit it
    
    Change-Id: I7ed0a45f5d7a04bb94501f0010a83c118bd84038

--- device.mk
+++ device.mk
@@ -15,6 +15,12 @@
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/htc/m7/m7-vendor.mk)
 
+# FM radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
 
